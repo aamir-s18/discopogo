4bdb5f0ac2c4ba4452d4c3b742e5d3969cc95e9a
aaxsh <aamirshakir0@gmail.com>
2022-12-03 21:48:51 +0100

    adding base

--- foodo_ts/src/components/HelloWorld.vue
+++ foodo_ts/src/components/HelloWorld.vue
@@ -1,57 +1,17 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
 </script>
-<script>
-export default {
-  data: function () {
-    return {
-      chartOptions: {
-        chart: {
-          id: "vuechart-example",
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-        },
 
-        grid: {
-          show: false
-        }
-
-      },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 35, 50, 49, 60, 70, 91],
-        },
-      ],
-    };
-  },
-};
-</script>
 <template>
-  <div>
-    <h1>👋 Hey foodlab alpine! </h1>
-    <div class="p2 bg-white rounded">
-      <h2><b>1010</b> Meals today!</h2>
-      <div style=" width:75%; margin: 0 auto;">
-        <apexchart class="chart" width="100%" type="bar" :options="chartOptions" :series="series">
-        </apexchart>
-      </div>
-
-
-
-    </div>
-    <!-- <h3>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3> -->
-
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
   </div>
 </template>
 
@@ -62,8 +22,7 @@ h1 {
   top: -10px;
 }
 
-
-👋🏾 h3 {
+h3 {
   font-size: 1.2rem;
 }
 
@@ -73,7 +32,6 @@ h1 {
 }
 
 @media (min-width: 1024px) {
-
   .greetings h1,
   .greetings h3 {
     text-align: left;
