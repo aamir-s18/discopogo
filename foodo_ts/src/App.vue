1c63d4124b87ec9a735e55891fbcaca68fcb67cd
Yurui <alexzyr@163.com>
2022-12-03 20:53:00 +0100

    update

--- foodo_ts/src/App.vue
+++ foodo_ts/src/App.vue
@@ -4,23 +4,17 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="max-width-4 mx-auto">
+    <div class="clearfix">
+      <HelloWorld />
     </div>
-  </header>
+  </div>
+
 
   <RouterView />
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -82,4 +76,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
--- foodo_ts/src/components/HelloWorld.vue
+++ foodo_ts/src/components/HelloWorld.vue
@@ -1,17 +1,57 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
 </script>
+<script>
+export default {
+  data: function () {
+    return {
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
 
+        grid: {
+          show: false
+        }
+
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 35, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+  },
+};
+</script>
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
+  <div>
+    <h1>👋 Hey foodlab alpine! </h1>
+    <div class="p2 bg-white rounded">
+      <h2><b>1010</b> Meals today!</h2>
+      <div style=" width:75%; margin: 0 auto;">
+        <apexchart class="chart" width="100%" type="bar" :options="chartOptions" :series="series">
+        </apexchart>
+      </div>
+
+
+
+    </div>
+    <!-- <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3> -->
+
   </div>
 </template>
 
@@ -22,7 +62,8 @@ h1 {
   top: -10px;
 }
 
-h3 {
+
+👋🏾 h3 {
   font-size: 1.2rem;
 }
 
@@ -32,6 +73,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
