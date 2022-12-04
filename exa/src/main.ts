dcd771f83c3b3ccbf960ea1b951ecb6979d3cd4c
jozefc <jozef-c@hotmail.com>
2022-12-04 07:59:22 +0100

    Added inference example to Tomorrow.vue

--- exa/src/main.ts
+++ exa/src/main.ts
@@ -2,14 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from '@/router/router'
 import VueApexCharts from "vue3-apexcharts";
-
-
+import { createPinia } from 'pinia';
+const pinia = createPinia()
+	
 
 import './assets/main.css'
 
 
-const app = createApp(App)
 
+const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.use(VueApexCharts)
 app.mount('#app')
--- exa/src/views/Tomorrow.vue
+++ exa/src/views/Tomorrow.vue
@@ -1,6 +1,58 @@
 <script>
+import { tf, browser, EmptyMemory, Memory, Path, Task, data, ModelType, client as clients, fetchTasks } from '@epfml/discojs'
+    import { watch, computed, shallowRef, onActivated, onMounted } from 'vue'
+    import { useMemoryStore } from '../memory'       
+    const memoryStore = useMemoryStore()
+    const memory = computed(() =>
+    memoryStore.useIndexedDB ? new browser.IndexedDB() : new EmptyMemory())
+    const serverUrl = new URL('', `http://localhost:8080`)
+    async function getDatas() {
+        const task = await FetchTask(serverUrl)
+        const source = {
+            type: ModelType.SAVED,
+            taskID: task.taskID,
+            name: task.trainingInformation.modelID
+        }
+        return [source, task]
+    }
+
+    async function FetchTask(serverUrl) {
+        try {
+            const tasks = await fetchTasks(serverUrl)
+            const foodTask = tasks.get("food")            
+            return foodTask
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const downloadModel = async (source, task) => {    
+        const client = new clients.Local(serverUrl, task)
+        const model = await client.getLatestModel()    
+        await memory.value.saveModel(source, model)
+        await memoryStore.initModels()
+    }
+
+    const getModel = async (source, task) => {
+        if (source !== undefined && await memory.value.contains(source)) {
+            return await memory.value.getModel(source)
+        }
+    }
+
+    const predict = async (source, task) => {
+        const model = await getModel(source, task)
+        const data = tf.tensor2d([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [12, 1])
+        const prediction = model.predict(data)
+        console.log(prediction)        
+    }
 
 export default {
+    beforeMount(){        
+        getDatas().then(([source, task]) => {
+            downloadModel(source, task)
+            predict(source, task)
+        })
+    },       
     data: function () {
         return {
             chartOptions: {
