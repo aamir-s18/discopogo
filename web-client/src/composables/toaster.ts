4fd4fb08ca28081fa0c87c1cbc0e477e7e007e59
aaxsh <aamirshakir0@gmail.com>
2022-12-04 09:25:16 +0100

    big clean

--- web-client/src/composables/toaster.ts
+++
@@ -1,5 +0,0 @@
-import { createToaster } from '@meforma/vue-toaster'
-
-const toaster = createToaster({ duration: 5000 })
-
-export const useToaster = () => toaster
--- web-client/src/config.ts
+++
@@ -1,7 +0,0 @@
-export interface Config {
-    readonly serverUrl: URL
-}
-
-export const CONFIG: Config = {
-  serverUrl: new URL('', process.env.VUE_APP_SERVER_URL)
-}
